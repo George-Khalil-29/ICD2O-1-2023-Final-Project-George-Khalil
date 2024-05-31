d7b4a16bf88c8ba7512b5931fbeaf9ebacc28388
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-05-31 01:39:19 +0000

    save the last changes in the chat bot

---
+++ js/responses.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2024 George Khalil All rights reserved
+//
+// Created by: George Khalil
+// Created on: May 2024
+// This file contains the JS functions for index.html
+
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "rock") {
+        return "paper";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!";
+    }
+}
