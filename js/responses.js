337745f917514a81ade3169d77d7bfc425ae88d2
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-05-31 18:46:14 +0000

    added more responses

--- js/responses.js
+++ js/responses.js
@@ -5,23 +5,43 @@
 // This file contains the JS functions for index.html
 
 function getBotResponse(input) {
-    //rock paper scissors
-    if (input == "rock") {
+    if (input === "rock") {
         return "paper";
-    } else if (input == "paper") {
+    } else if (input === "paper") {
         return "scissors";
-    } else if (input == "scissors") {
+    } else if (input === "scissors") {
         return "rock";
-    }
-
-    // Simple responses
-    if (input == "hello") {
+    } else if (input === "hello") {
         return "Hello there!";
-    } else if (input == "goodbye") {
+    } else if (input === "goodbye") {
         return "Talk to you later!";
+    } else if (input === "fun fact") {
+        const funFacts = [
+            "The shortest war in history lasted only 38 minutes between Britain and Zanzibar in 1896.",
+            "A group of flamingos is called a flamboyance.",
+            "Honey never spoils. Archaeologists have found pots of honey in ancient Egyptian tombs that are over 3,000 years old and still perfectly edible.",
+            "The Eiffel Tower can be 15 cm taller during the summer due to thermal expansion of the iron.",
+            "Octopuses have three hearts.",
+        ];
+        const randomIndex = Math.floor(Math.random() * funFacts.length);
+        return funFacts[randomIndex];
+    } else if (input === "random number") {
+        const randomNumber = Math.floor(Math.random() * 100) + 1;
+        return `Your random number is ${randomNumber}.`;
+    } else if (input.startsWith("count to")) {
+        const numberToCount = parseInt(input.split(" ")[2]);
+        if (!isNaN(numberToCount)) {
+            let count = "";
+            for (let i = 1; i <= numberToCount; i++) {
+                count += i + ", ";
+            }
+            return count.slice(0, -2);
+        }
+    } else if (input === "time") {
+        const now = new Date();
+        const timeString = now.toLocaleTimeString();
+        return `The current time is ${timeString}.`;
     } else {
-        return "Try asking something else!";
+        return "I'm sorry, I didn't understand that. Try asking something else!";
     }
-
-  
 }
